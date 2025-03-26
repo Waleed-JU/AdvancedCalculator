e8db3f34337d5e6275cc190b636b4ae1c0c22fa4
Waleed-JU <wgalhumud@ju.edu.sa>
2025-03-27 01:38:11 +0300

    Update Subtract.java

--- src/main/java/com/calculator/advancedcalculator/Subtract.java
+++ src/main/java/com/calculator/advancedcalculator/Subtract.java
@@ -2,6 +2,6 @@ package com.calculator.advancedcalculator;
 
 public class Subtract {
     public double calculate(double a, double b) {
-        return a - b;
+        return a - b; // to return the value
     }
 }
